a3ca1634fe3710a940c0383d329777dc78622498
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2014-01-23 17:42:44 -0600

    Adding validation constraint checks

--- src/main/groovy/org/zirbes/groovy/validation/Bicycle.groovy
+++ src/main/groovy/org/zirbes/groovy/validation/Bicycle.groovy
@@ -1,19 +1,30 @@
 package org.zirbes.groovy.validation
 
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotNull
+
 class Bicycle {
 
+    @NotNull
     String manufacturer
 
+    @NotNull
     String model
 
+    @Min(16L)
     Integer wheelSize
 
     MeasurementType wheelMeasurement
 
     TireType tireType
 
+    @Min(13L)
+    @Max(62L)
+    @NotNull
     Integer frameSize
 
+    @NotNull
     MeasurementType frameMeasurement
 
     String toString() {
--- src/test/groovy/org/zirbes/groovy/validation/BicycleSpec.groovy
+++ src/test/groovy/org/zirbes/groovy/validation/BicycleSpec.groovy
@@ -1,8 +1,23 @@
 package org.zirbes.groovy.validation
 
+import java.util.Set
+
+import javax.validation.ConstraintViolation
+import javax.validation.Validation
+import javax.validation.Validator
+import javax.validation.ValidatorFactory
+
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class BicycleSpec extends Specification {
+
+    private static Validator validator
+
+    void setup() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory()
+        validator = factory.getValidator()
+    }
 
     void 'we can build a bicycle'() {
         given:
@@ -21,6 +36,41 @@ class BicycleSpec extends Specification {
 
         then:
         description == '40cm Surly Steamroller'
+    }
+
+    void 'a bicycle must have a make, model and frame size'() {
+        given:
+        Bicycle bike = new Bicycle()
+
+        when:
+        Set<ConstraintViolation<Bicycle>> violations = validator.validate(bike)
+
+        then:
+        violations.size() == 4
+    }
+
+    @Unroll
+    void 'a bicycle that meets the minumum requirements for frame size #frame'() {
+        given:
+        Bicycle bike = new Bicycle(
+            manufacturer: 'Surly',
+            model: 'Steamroller',
+            frameSize: frame,
+            frameMeasurement: MeasurementType.CM
+        )
+
+        when:
+        Set<ConstraintViolation<Bicycle>> violations = validator.validate(bike)
+
+        then:
+        violations.size() == errors
+        !message || message == violations.iterator().next().message
+
+        where:
+        frame || errors | message
+        10    || 1      | 'must be greater than or equal to 13'
+        40    || 0      | ''
+        80    || 1      | 'must be less than or equal to 62'
     }
 }
 
